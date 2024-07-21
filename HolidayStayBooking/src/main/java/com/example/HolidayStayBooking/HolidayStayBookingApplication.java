96de0db3d28cea96d71596e4dac31006e7b00d48
Shintu kumar <shintukumar9128@gmail.com>
2024-07-21 14:45:29 +0530

    Holiday Project

---
+++ HolidayStayBooking/src/main/java/com/example/HolidayStayBooking/HolidayStayBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.HolidayStayBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayStayBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayStayBookingApplication.class, args);
+	}
+
+}
---
+++ HolidayStayBooking/src/test/java/com/example/HolidayStayBooking/HolidayStayBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HolidayStayBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayStayBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
